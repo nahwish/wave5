0569284eab107ebfcb5888305b683433aa6952ce
chawi rojas <nadaro@outlook.com>
2022-07-04 18:26:54 -0300

    clase1-express

---
+++ express-clase1/ejercitacion/routes/Get.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+
+/************************************************************* 
+	EJERCICIO 
+	1-Crear un endpoint que utilice el método GET y 
+	devuelva como respuesta el mensaje “¡Hola mundo!”. 
+	2-Probar que el mensaje llega al navegador.
+	http://localhost:3000/api/
+ ************************************************************/
+// Tu código acá
+
+
+
+
+
+/*************************************************************
+	EXTRA
+	1-Crear un array de productos usando la variable products
+	2-Crear otro método Get cuya ruta sea '/products'
+	3-Responder con el array al ingresar a '/products'
+	http://localhost:3000/api/products
+ *************************************************************/
+// Tu código acá
+let products;
+
+
+
+
+
+//expressjs.com/es/starter/hello-world.html
+module.exports = { router, products };
---
+++ express-clase1/ejercitacion/utils/utils.js
@@ -0,0 +1,6 @@
+function searchingLengthProperty(products, key) {
+	let lengthResult = products.filter((x) => x[key]).length;
+
+	return lengthResult;
+}
+module.exports = searchingLengthProperty;
