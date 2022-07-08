e02708327558147f7e27370b303d5b5c9033a106
chawi rojas <nadaro@outlook.com>
2022-07-08 12:55:01 -0300

    se creo la carpeta de react-clase2

---
+++ React/src/1-Semantico/clase2/homework/form.js
@@ -0,0 +1,30 @@
+const React = require("react");
+
+/**
+ *********************Punto 1************************
+ Crear 3 inputs (name,email y phone) con sus labels correspondientes
+ 
+ *********************Punto 2*************************
+ Crear un select cuyo label sea 'Countries'
+ las opciones deben ser (Argentina, Chile, Colombia, Venezuela, México, Bolivia,Uruguay) 
+ 
+ *********************Punto 3************************
+ Crear dos radio button (envio y retiro) con sus labels correspondientes
+ 
+ *********************Punto 4************************
+ Crear al menos 2 checkbox (Reloj y Notebook) con sus labels correspondientes
+ */
+
+
+
+function Form() {
+	return (
+		<>
+			<form>
+				
+			</form>
+		</>
+	);
+}
+
+export default Form
