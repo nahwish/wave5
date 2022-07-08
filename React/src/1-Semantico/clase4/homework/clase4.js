214a42e2305865cbe0afe0d9c765e4e266679646
chawi rojas <nadaro@outlook.com>
2022-07-08 13:46:03 -0300

    se creó la carpeta 4 de react

---
+++ React/src/1-Semantico/clase4/homework/clase4.js
@@ -0,0 +1,26 @@
+const React = require("react");
+/**************************************************************************
+ * EJERCICIO INTEGRADOR
+ * 1- Implementar inputs para los campos 'Nombre','Apellido' y 'Teléfono'
+ * 2-Implementar un Select con las opciones de paises: "Argentina",
+	"Uruguay","Chile","Colombia","México DF","Venezuela","Bolivia".
+ * 3-Implementar un select con localidades, pondremos como opciones : 
+	"Buenos Aires","Santiago","Bogotá","México DF","Caracas","Sucre",
+	"Montevideo".
+	4-Implementar inputs para los campos 'Dirección','Número' y 'CP'.
+	5-Implementar inputs tipo checkbox para los campos 'Auto','Hogar' y 'Ropa'.
+	6-Implementar inputs para los campos 'Email','Contraseña' y 'Confirmar'.
+	7-Implementar inputs tipo radio para los campos 'Residencial' y 'Laboral'.
+***************************************************************************/
+
+function Integrador() {
+	return (
+		<>
+			<form>
+			
+			</form>
+		</>
+	);
+}
+
+export default Integrador;
