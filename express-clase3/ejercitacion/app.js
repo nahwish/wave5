e88478d2b39525ff2ccb601124f2a0a9f0a22a86
chawi rojas <nadaro@outlook.com>
2022-07-27 12:30:32 -0300

    cambio de nombre de carpetas

--- express-clase3/ejercitacion/app.js
+++ express-clase3/ejercitacion/app.js
@@ -1,17 +1,28 @@
-
 const express = require("express");
-const {router} = require('./routes/products')
 
-
-const PORT = 3000;
+const {router}  = require("./routes/products");
 
 const app = express();
+const PORT = 3000
 
-app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
+// definimos una ruta base
 app.use("/api", router);
 
 
-app.listen(PORT, console.log(`Listening on http://localhost:${PORT}`));
+app.use((req, res) => {
+	res.status(404).send("Error 404, page not found");
 
-module.exports = { app };
+});
+
+
+let responseServer = app.listen(
+	PORT,
+	console.log(`Listening on http://localhost:${PORT}`)
+);
+
+module.exports = {
+	app,
+	responseServer,
+};
